8b306d78d391b9e81bc92301687e00162387e957
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-08-16 22:56:32 +0000

    chore: Add initial project files and configuration

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-import express from 'express';
-import configureHttpServer from './services/httpserver.js';
+/**
+ * @format
+ */
 
-// Create express app
-const app = express();
+import 'react-native-get-random-values';
+import {AppRegistry} from 'react-native';
+import {AppWrapper} from './source/AppWrapper';
+import {name as appName} from './app.json';
 
-// Establish port
-const port = process.env.PORT || 8080;
-
-// Global Error Handler
-const onGlobalErrors = (error) => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-// Create web server
-configureHttpServer(app).then((webserver) => {
-  webserver.on('error', onGlobalErrors);
-  webserver.listen(port, '0.0.0.0', () => console.log(`Server listening on port: ${port}`));
-});
+AppRegistry.registerComponent(appName, () => AppWrapper);
