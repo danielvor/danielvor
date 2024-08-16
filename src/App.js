1a04fe70c2d202c9e8cd5ae0b8fc9d38d129a669
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-08-16 02:01:56 +0000

    chore: Add initial project files and configuration

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Daniel Rodrigues is a <code>Software Engineer</code> and <code>Full Stack Developer</code> with a passion for <code>Web Development</code> and <code>Mobile Development</code>.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
