103ba545862f25b0c74d44964e17b73825c0dfdb
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-08-23 04:22:24 +0000

    chore: Update next.config.js and tsconfig.json files

--- next.config.tsx
+++
@@ -1,6 +0,0 @@
-// next.config.js
-module.exports = {
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/danielvor/' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/danielvor' : '',
-  }
-  
