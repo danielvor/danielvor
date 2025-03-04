641e7f7cce7d981559392065e43a66e500438e10
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2025-03-03 21:50:08 -0300

    main

--- danielvor/Program.cs
+++ danielvor/Program.cs
@@ -18,9 +18,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
