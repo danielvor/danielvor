c1b93340e1c6e1b38fd27681cb61a1f39b936756
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2025-03-06 00:08:46 +0000

    portfolio django exemplo

--- danielvor/danielvor/settings.py
+++ danielvor/danielvor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d5p-tk)mr$-c4+mm_qgqin0iav!$-hs_(ruzn5ia5a-13mc*v-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
