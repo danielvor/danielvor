e73551bc1eeedc09913e647999f04f7e7288016e
Daniel Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2025-03-12 20:53:27 +0000

    blazor app init

--- danielvor/wwwroot/lib/bootstrap/dist/js/bootstrap.esm.js
+++ danielvor/wwwroot/lib/bootstrap/dist/js/bootstrap.esm.js
@@ -1124,7 +1124,7 @@ const KEY_TO_DIRECTION = {
   [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
 };
 const Default$b = {
-  interval: 5042,
+  interval: 5046,
   keyboard: true,
   pause: 'hover',
   ride: false,
@@ -4294,7 +4294,7 @@ const DefaultType = {
 const Default = {
   animation: true,
   autohide: true,
-  delay: 5042
+  delay: 5046
 };
 
 /**
